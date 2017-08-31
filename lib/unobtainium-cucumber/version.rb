be5705f6e08e4edfad3886a1d00afdf8a9f7e331
Jens Finkhaeuser <jens@finkhaeuser.de>
2017-08-31 12:57:20 +0200

    Bump copyright notice

--- lib/unobtainium-cucumber/version.rb
+++ lib/unobtainium-cucumber/version.rb
@@ -3,12 +3,12 @@
 # unobtainium-cucumber
 # https://github.com/jfinkhaeuser/unobtainium-cucumber
 #
-# Copyright (c) 2016 Jens Finkhaeuser and other unobtainium-cucumber
+# Copyright (c) 2016-2017 Jens Finkhaeuser and other unobtainium-cucumber
 # contributors. All rights reserved.
 #
 module Unobtainium
   module Cucumber
     # The current release version
-    VERSION = "0.3.0".freeze
+    VERSION = "0.4.0".freeze
   end # module Cucumber
 end # module Unobtainium
